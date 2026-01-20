6c01da4010dd04b89c1a326605dcbeb40e24de8a
Anup9621 <tiwarianup1142116@gmail.com>
2026-01-21 00:33:16 +0530

    changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', "localhost"]
+ALLOWED_HOSTS = ["74.220.48.0/24"]
 
 # Application definition
 INSTALLED_APPS = [
