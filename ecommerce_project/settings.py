ef896bbfe2dd658e77f1704b2d962d2ae4d5b30d
Anup9621 <tiwarianup1142116@gmail.com>
2026-01-21 00:58:36 +0530

    changes

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['kommertio9.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
